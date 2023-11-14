f86a0de07646a47494345c00fb759dbeac2f0a12
James Hagin <121306046+Jameshagin28@users.noreply.github.com>
2023-11-14 04:03:16 -0800

    js style

--- js/index.js
+++ js/index.js
@@ -8,20 +8,3 @@ function toggleNav() {
 
 
 //-----------ourTeam Flip Cards-----///
-const inputs = document.querySelectorAll(".input");
-
-function focusFunc() {
-    let parent = this.parentNode;
-    parent.classList.add("focus");
-}
-
-function blurFunc() {
-    let parent = this.parentNode;
-    if (this.value == "") {
-        parent.classList.remove("focus");
-    }
-}
-inputs.forEach((input) => {
-    input.addEventListener("focus", focusFunc);
-    input.addEventListener("blur", blurFunc);
-});
